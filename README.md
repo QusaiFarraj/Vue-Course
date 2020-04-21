6bcfa19b939802d14831e4f2eacf241aea4df662
Qusai Farraj <qusai919@gmail.com>
2020-04-21 08:55:55 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Vue-Course
-Created with CodeSandbox
---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image:
+      "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+    imageAlt: "",
+    inventory: 100,
+    inStock: true
+  }
+});
